4e2508785aca4ba1c792eca0fa89ca191b30dd89
lenilsamuel <lenilvarghese@gmail.com>
2021-12-20 20:30:11 -0700

    add parameterized query to protect against sql injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,24 +1,28 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: 'vagrant',
-  password: '123',
-  host: 'localhost',
-  database: 'bootcampx'
+  user: "vagrant",
+  password: "123",
+  host: "localhost",
+  database: "bootcampx",
 });
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+const queryString = `SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
-.then((res) => {
-  res.rows.forEach((row) => {
-    console.log(`${row.cohort}: ${row.teacher}`);
-  })
-}).catch((err) => console.log('query error', err.stack ));
+WHERE cohorts.name = $1
+ORDER BY teacher;`;
 
+const cohortName = process.argv[2] || "JUL02";
+const values = [`${cohortName}`];
+
+pool
+  .query(queryString, values)
+  .then((res) => {
+    res.rows.forEach((row) => {
+      console.log(`${row.cohort}: ${row.teacher}`);
+    });
+  })
+  .catch((err) => console.log("query error", err.stack));
